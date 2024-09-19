ce385c2a3be2a88569dff4edd877e962cf738625
Николай <nikolayivanov944@gmail.com>
2024-09-20 00:49:29 +0300

    Init

--- src/Main.java
+++ src/Main.java
@@ -53,18 +53,3 @@ public class Main {
 
     }
 }
-
-
-
-//        System.out.println("Задание 3");
-//        int deliveryDistance = 95;
-//        if (deliveryDistance <= 20) {
-//            System.out.println("Потребуется дней : 1");
-//        } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
-//            System.out.println("Потребуется дней : 2");
-//        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
-//            System.out.println("Потребуется дней : 3");
-//        } else {
-//            System.out.println("Доставки свыше 100 км доставки нет");
-//        }
-//        System.out.println();
